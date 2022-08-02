6268c74f7bb8dfa8d3a19264f42c61d73a10ce87
sa-mamun <samamun009@gmail.com>
2022-08-02 22:31:50 +0600

    problem solving code added

---
+++ DefanginAnIPAddress/DefanginAnIPAddress/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DefanginAnIPAddress
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            string[] values = input.Split('.');
+            string a = string.Empty;
+            for (int i = 0; i< values.Length; i++)
+            {
+                if (i != values.Length - 1)
+                    a += values[i] + "[.]";
+                else
+                    a += values[i];
+            }
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LC_2011/LC_2011/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LC_2011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 0;
+            var input = Console.ReadLine();
+            string[] operations = input.Split(',');
+            if (operations.Length >= 1 && operations.Length <= 100)
+            {
+                for (int i = 0; i < operations.Length; i++)
+                {
+                    if (operations[i].Contains("++X") || operations[i].Contains("X++"))
+                        X++;
+                    else if (operations[i].Contains("--X") || operations[i].Contains("X--"))
+                        X--;
+                }
+            }
+            Console.WriteLine(X);
+            Console.ReadLine();
+        }
+    }
+}
