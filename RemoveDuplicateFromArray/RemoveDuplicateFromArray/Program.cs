acdd2f2eb9ab293b25fed5e48c074f72f908cf8e
samir <samamun009@gmail.com>
2023-12-02 21:03:12 +0600

    Remove Duplicate from array problem solved

---
+++ RemoveDuplicateFromArray/RemoveDuplicateFromArray/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace RemoveDuplicateFromArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int k = 1;
+            int pointerValue = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != pointerValue)
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+                pointerValue = nums[i];
+            }
+
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
