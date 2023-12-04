edc1e9ed5005cd4a8d6c352bf220043ed69ea2c3
samir <samamun009@gmail.com>
2023-12-05 00:14:08 +0600

    Leetcode medium array problem solved

---
+++ ContainerWithMostWater/ContainerWithMostWater/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ContainerWithMostWater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            int maxArea = 0;
+            int localMaxArea = 0;
+            int i = 0;
+            int j = height.Length - 1;
+
+            while ( i < j)
+            {
+                if (height[i] < height[j])
+                {
+                    localMaxArea = height[i] * (j - i);
+                    i++;
+                }
+                else
+                {
+                    localMaxArea = height[j] * (j - i);
+                    j--;
+                }
+
+                if (localMaxArea > maxArea)
+                    maxArea = localMaxArea;
+            }
+
+            Console.WriteLine(maxArea.ToString());
+        }
+    }
+}
